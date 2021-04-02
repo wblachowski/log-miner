6702f6d0b79c59bf525cc077453da4c70f6828ec
Wojciech Blachowski <wblachowsky@gmail.com>
2021-04-02 23:22:14 +0200

    Remove the aem endpoint

--- api/app.py
+++ api/app.py
@@ -16,23 +16,5 @@ def cluster_from_file():
     return jsonify(json.dumps(clusters, default=lambda o: o.__dict__))
 
 
-@app.route('/api/aem/fetch', methods=['POST'])
-@cross_origin(origins='*', send_wildcard=True)
-def fetch_from_aem():
-    print("Xd")
-    print(request.form)
-    url = request.form['url']
-    user = request.form['user']
-    pwd = request.form['pwd']
-    addr = f'http://{user}:{pwd}@{url}/system/console/slinglog/tailer.txt?name=%2Flogs%2Ferror.log'
-    print(addr)
-    logs = requests.get(addr).text
-    logs = '\n'.join([log for log in logs.split('\n') if 'ERROR' in log])
-    print("gotit")
-    print(logs)
-    clusters = get_clusters(logs)
-    return jsonify(json.dumps(clusters, default=lambda o: o.__dict__))
-
-
 if __name__ == '__main__':
     app.run(host='localhost')
